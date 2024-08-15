b48b5ab48a0b0ca537dd6f13e0da80b3245def51
SAGAR T <ssagu6661@gmail.com>
2024-08-15 10:55:25 -0400

    Add files via upload

---
+++ accout datails input.py
@@ -0,0 +1,18 @@
+#   import phonenumbers
+#   from phonenumbers import geocoder 
+#   phone_number1= phonenumbers.parase("+917294536271")
+#  phone_number2= phonenumbers.parase("+918878586271")
+#   phone_number3= phonenumbers.parase("+12136574429")
+#    phone_number4= phonenumbers.parase("+201234567898")
+
+#   prnt("\nphone Numbers Location\n")
+#   print(geocoder.description_for_number(phone_number1,"en"))
+#    print(geocoder.description_for_number(phone_number2,"en"))
+#     print(geocoder.description_for_number(phone_number3,"en"))
+#      print(geocoder.description_for_number(phone_number4,"en")
+
+print("ENTER ACCOUNT DETAILS FOR EKYC")
+name=input("Enter Your Name = ")
+accno = input("Enter Account Number = ")
+ifsc =  input("Enter IFSC Number = ")
+print ("KYC Done yes successfully",name)
